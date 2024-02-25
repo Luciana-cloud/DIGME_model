e9bcf711ae1b709eb9584abf4d9d5893ee37b1ce
Luciana-cloud <lucianagoku@hotmail.com>
2024-02-25 00:59:03 -0800

    Test for statistical model between respiration and moisture and soil properties.

--- test_models.R
+++ test_models.R
@@ -9,6 +9,12 @@ library("ggpmisc")
 library("ggpubr")
 library("plotly")
 library("readxl")
+library("lme4")
+library("car")
+library("MuMIn")
+library("cAIC4")
+library("buildmer")
+library("lmerTest")
 
 # Called data ----
 
@@ -36,9 +42,47 @@ DIGME_mean$SiteCode[DIGME_mean$SiteCode == "brookdale.ca"] = "brookdale"
 
 DIGME_mean.2 = DIGME_mean[,3:15]
 
-# Matlab database
+# Matlab database ---- 
 
 write.csv(DIGME_mean, file="DIGME_mean.csv",col.names = F, row.names = F)
 write.table(DIGME_mean.2, file="DIGME_mean.2.txt",col.names = F, row.names = F)
+
+#  Statistical model ----
+
+DIGME_clean.2 = DIGME_data[-c(7,14,21,28,35,42,49,56,63,70,77,84,91,98,105,112,
+                            119,126),]
+DIGME_clean.2 = DIGME_clean.2[c("SiteCode","RainTrt","FieldRep","CoreNum",
+                            "ActualGWC","BD","Moist","DOC","MBC","SOM","Clay",
+                            "CO2_eq")]
+DIGME_clean.2$Clay = DIGME_clean.2$Clay/100
+DIGME_clean.2      = DIGME_clean.2 %>% mutate(SOC = as.numeric(SOM)/1.72)
+
+SR = lmer(log(as.numeric(CO2_eq)) ~ as.numeric(ActualGWC) + as.numeric(ActualGWC)^2 + 
+            as.numeric(ActualGWC)^3 + (BD+Clay+SOC)*as.numeric(ActualGWC) + 
+            (1|RainTrt/SiteCode),data = DIGME_clean.2)
+summary(SR)
+Anova(SR)
+cAIC(SR)
+AIC(SR)
+r.squaredGLMM(SR)
+
+qqnorm(residuals(SR))
+scatter.smooth(residuals(SR) ~ fitted(SR))
+
+# Model reduction ----
+
+SR.2 = lmer(log(as.numeric(CO2_eq)) ~ as.numeric(ActualGWC) + as.numeric(ActualGWC)^2 + 
+            as.numeric(ActualGWC)^3 + BD + Clay + 
+            (1|RainTrt/SiteCode),data = DIGME_clean.2)
+summary(SR.2)
+Anova(SR.2)
+cAIC(SR.2)
+AIC(SR.2)
+r.squaredGLMM(SR.2)
+
+qqnorm(residuals(SR.2))
+scatter.smooth(residuals(SR.2) ~ fitted(SR.2))
+
+
 
 
